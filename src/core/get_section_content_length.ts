45267c9b0c5aa1602d90d8613033ec8a6532a978
grind-t <grindt.gd@gmail.com>
2026-02-14 01:48:41 +0300

    Update get_section_content_length.ts

--- src/core/get_section_content_length.ts
+++ src/core/get_section_content_length.ts
@@ -1,6 +1,17 @@
-import { getBlockLength } from "./get_block_length.ts";
-import type { Section } from "./types.ts";
+import type { Root } from "mdast";
+import { iterSectionBlocks } from "./iter_section_blocks.ts";
 
-export function getSectionContentLength(section: Section): number {
-  return section.content.reduce((acc, v) => acc + getBlockLength(v), 0);
+export type GetSectionContentLengthInput = {
+  ast: Root;
+  headingIndex: number;
+};
+
+export function getSectionContentLength(
+  input: GetSectionContentLengthInput,
+): number {
+  const blocks = Array.from(iterSectionBlocks(input));
+  const start = blocks.at(0)?.position?.start.offset ?? 0;
+  const end = blocks.at(-1)?.position?.end.offset ?? 0;
+
+  return end - start;
 }
